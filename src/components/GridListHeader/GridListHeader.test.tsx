3ecea0a3694a7ccd5e3e02153ef62205a22d8c33
apple <davidhiggins1712@gmail.com>
2020-08-26 20:08:06 +1000

    --no-edit

---
+++ src/components/GridListHeader/GridListHeader.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render , screen} from "@testing-library/react"
+import GridListHeader from "./Index"
+
+describe('GridListHeader', () => {
+  it("renders title", () => {
+    render(<GridListHeader title={"Test title"} />)
+    expect(screen.getByText("Test title")).toBeInTheDocument()
+  })
+})
---
+++ src/components/GridListHeader/Index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type GridListHeaderProps = {
+  title: string
+}
+
+const GridListHeader: React.FC<GridListHeaderProps> = ({title}) => {
+  return (
+    <h3>
+      {title}
+    </h3>
+  )
+}
+
+export default GridListHeader
